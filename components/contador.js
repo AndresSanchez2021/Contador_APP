7789e205b2200d9d35ec9905ce0d6003a53dc560
AndresSanchez2021 <anfsanchezcu@unal.edu.co>
2022-06-13 00:08:46 -0500

    contador terminado (manejo de estados)

--- components/contador.js
+++ components/contador.js
@@ -1,19 +1,17 @@
 const Contador=()=>{
     const[counter, setCounter] = React.useState(0);
+    const aumentar = ()=> setCounter(counter + 1)
+    const disminuir = ()=> setCounter(counter - 1)
+    const color =()=>counter < 1 ? "positivo":"negativo";
     return(
-        <div className="container bt-5">
-            <div className="row  justify-content-center">
-                <div className="col-4 text-center">
-                    <h1>Counter</h1>
-                    <div className="row justify-content-around">
-                        <div className="col-6">
-                            <button className="btn btn-lg btn-outline-primary mr-10">Start</button>
-                        </div>
-                        <div className="col-6">
-                            <button className="btn btn-lg btn-outline-danger ml-10">Stop</button>
-                        </div>
-                        
-                        
+        
+        <div className="container d-flex justify-content-center align-items-center">
+            <div className="row">
+                <div className="col-12 col-md-10 col-lg-6 text-center">
+                    <h1 className={color()}>{counter} </h1>
+                    <div className="d-flex justify-content-around">
+                        <button className="btn btn-lg btn-outline-primary mr-10" onClick={aumentar}>Increase</button>
+                        <button className="btn btn-lg btn-outline-danger ml-10" onClick={disminuir}>Diminish</button>
                     </div>
                 </div>
             </div>
